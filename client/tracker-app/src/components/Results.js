88b5efaaf6282d80ff64d256941d4c414902e543
Ryan Bettis <rbettis2@asu.edu>
2019-11-13 00:10:10 -0700

    added platform

--- client/tracker-app/src/components/Results.js
+++ client/tracker-app/src/components/Results.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 
 
@@ -26,34 +27,55 @@ class Results extends Component {
         
     }
     
+    getStyle = () => {
+        return {
+            background: '#f4f4f4',
+            padding: '10px',
+            borderBottom: '1px #ccc dotted'
+        }
+    }
 
     render() {
 
+
+        
         
         //const { name, cover } = this.props.searchResults;
-        const games2 = this.props.searchResults.map((game, index) => {
+        const games2 = this.props.searchResults.map((game) => {
             
             const imageUrl = game.cover ? 'https://images.igdb.com/igdb/image/upload/t_cover_small/' + game.cover.image_id + '.jpg' : 'https://upload.wikimedia.org/wikipedia/commons/thumb/a/ac/No_image_available.svg/300px-No_image_available.svg.png';
+            
             var gameName = game["name"];
 
-            console.log(this.state.searchResults);
+            //console.log(this.state.searchResults);
             return (
                 <React.Fragment>
                     <p>
                         <img key={gameName + 'Cover'} src={imageUrl} style={{width:75, height:100}} alt="cover"></img>
+                        <br/>
                         <button key={gameName} id={gameName} onClick={e => this.addGame(gameName, imageUrl)} >{gameName}</button>
-                        
+                        <br/>
+                        { game.platforms !== undefined ? game.platforms.map((abbr) => {
+                            return (
+                                        <React.Fragment>
+                                                <input type="checkbox" value={ abbr.abbreviation } />
+                                                <label>{abbr.abbreviation + " " }</label>
+                                        </React.Fragment>
+                                    )
+                            }) : <label>No Platform</label>}
+                            <br/><br/><br/>
                     </p>
                 </React.Fragment>
             )
         });
 
         return(
-            <div>
+            <div style={this.getStyle()}>
                 <h1>Results</h1>
                 <br></br>
                 <h2><label>{this.state.added}</label></h2>
-                {/* console.log(Object.values(this.props.searchResults)) */}
+                { /* console.dir( this.props.searchResults[0].name) */ }
+                
 
                 { games2 }
 
@@ -62,6 +84,12 @@ class Results extends Component {
         );
     }
 }
+
+// PropTypes
+Results.propTypes = {
+    searchResults: PropTypes.array.isRequired
+}
+
 
 
 export default Results;
---
+++ client/tracker-app/src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class header extends Component {
+    render() {
+        return (
+            <div>
+                <p>Header</p>
+            </div>
+        )
+    }
+}
+
+export default header
